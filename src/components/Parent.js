3c73bd2ccd5e527a9c0bcc78d29a6a4b536bcec3
Trimbak Pakhale <118504736+Trimbak15@users.noreply.github.com>
2023-06-23 11:03:49 +0000

    first

---
+++ src/components/Parent.js
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+
+const Parent = () => {
+  const [todos, setTodos] = useState([
+    {
+        id:1,
+        title: "Learn React",
+    },
+    {
+        id:2,
+        title : "Build React App"
+    },
+    {
+        id:3,
+        title: "Deploy react App"
+    },
+  ]);
+
+  const handleCompleteTodo = (todoId) => {
+    setTodos((prevTodos) =>
+      prevTodos.map((todo) => {
+        if (todo.id === todoId) {
+          return { ...todo, completed: true };
+        }
+        return todo;
+      })
+    );
+  };
+
+  return (
+    <div className="parent">
+      <h1>Parent Component</h1>
+      <Child todos={todos} handleCompleteTodo={handleCompleteTodo} />
+    </div>
+  );
+};
+
+const Child = ({ todos, handleCompleteTodo }) => {
+  return (
+    <div className="child">
+      <h2>Child Component</h2>
+      {todos.map((todo) => (
+        <div key={todo.id} className="todo">
+          <ul>
+            <li>
+            <span>{todo.title}</span>
+            <button
+            onClick={() => handleCompleteTodo(todo.id)}
+            hidden={todo.completed}>Complete</button>
+            </li>
+          </ul>
+          
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Parent;
